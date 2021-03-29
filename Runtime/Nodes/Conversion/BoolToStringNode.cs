71a02b8bbca709c4bb0e6476b1c9b1b3a1e55f20
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-03-30 01:52:29 +0200

    Working on scripting

---
+++ Runtime/Nodes/Conversion/BoolToStringNode.cs
@@ -0,0 +1,34 @@
+﻿using Juce.Logic.Graphs;
+using Juce.Scripting;
+using Juce.Scripting.Instructions;
+using Juce.Logic.Attributes;
+using System;
+
+namespace Juce.Logic.Nodes
+{
+    [LogicNode(
+        "Bool to String",
+        "Conversion",
+        new Type[] { typeof(BaseLogicGraph) })
+        ]
+    public class BoolToStringNode : LogicNode
+    {
+        [Input(connectionType = ConnectionType.Override, typeConstraint = TypeConstraint.Strict)]
+        public bool ValueIn;
+
+        [Output(connectionType = ConnectionType.Multiple, typeConstraint = TypeConstraint.Strict)]
+        public string ValueOut;
+
+        protected override void LinkScriptPorts()
+        {
+            LinkInputPortWithLogicPort(nameof(ValueIn), BoolToStringInstruction.ValueIn, ValueIn);
+            LinkOutputPortWithLogicPort(nameof(ValueOut), BoolToStringInstruction.ValueOut);
+        }
+
+        protected override ScriptInstruction GenerateInstruction(Script script)
+        {
+            return script.CreateScriptInstruction<BoolToStringInstruction>();
+        }
+
+    }
+}
